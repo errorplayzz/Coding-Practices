d0c4acb921ceecda66c782c35f5947adc385a788
errorplayzz <shashankkmahariyaa@gmail.com>
2026-06-19 00:47:16 +0530

    data structure session

---
+++ python/Syntax basics/escape_seq.py
@@ -0,0 +1,9 @@
+a = "error is best programer\nand learn some new stuff"
+
+b = "error is best programer\nand learn some new \"stuff\""
+
+c = "error\\programer"
+
+print(a)
+print(b)
+print(c)
---
+++ python/data structures/list.py
@@ -0,0 +1,7 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print(friends[1])
+
+friends[0] = "Grapes"
+
+print(friends[0])
