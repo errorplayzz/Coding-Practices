47e616ad30231ce94a67fdf5c8e5aff0a3b3db4d
errorplayzz <shashankkmahariyaa@gmail.com>
2026-06-21 20:44:16 +0530

    loop session

---
+++ python/01_Syntax basics/list_loop.py
@@ -0,0 +1,8 @@
+l = ["Error", False, 404, "Not Found", 1, 10.5, 356, "Python"]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    
+    i += 1
---
+++ python/01_Syntax basics/loop-steps.py
@@ -0,0 +1,13 @@
+#using for loop
+print("Using for loop")
+
+for i in range(0, 100,  5):
+    print(i)
+
+#using while loop
+print("Using while loop")
+
+i = 0
+while(i<100):
+    print(i)
+    i += 5
